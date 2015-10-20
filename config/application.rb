e1ce26be9a08496011dcd135a0c0140c9315f116
kgeographer <karlg@kgeographer.org>
2015-10-19 23:40:51 -0700

    4.2.4 seems stable, upgraded for asset paths

--- config/application.rb
+++ config/application.rb
@@ -3,11 +3,8 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 require 'dotenv-rails'
-# config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-
 Bundler.require(*Rails.groups)
 
 module Authorial
@@ -15,6 +12,7 @@ module Authorial
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -27,6 +25,5 @@ module Authorial
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
     config.assets.debug = true
-
   end
 end
