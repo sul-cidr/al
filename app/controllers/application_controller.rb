d633384f15d7c823d0f6dd304f6b8673a44e7bc9
kgeographer <karlg@kgeographer.org>
2016-03-31 18:44:46 -0700

    pre-deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with(
-    name: ENV['AL_USERNAME'],
-    password: ENV['AL_PASS'],
-  )
+  # http_basic_authenticate_with(
+  #   name: ENV['AL_USERNAME'],
+  #   password: ENV['AL_PASS']
+  # )
 
 end
