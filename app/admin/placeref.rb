b7cdf174de18f5a79ba5aa7eac0187df5485ff3b
kgeographer <karlg@kgeographer.org>
2016-06-07 17:58:31 -0700

    add year to placeref correctly this time

--- app/admin/placeref.rb
+++ app/admin/placeref.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Placeref do
   permit_params :placeref_id, :placeref, :work_id, :author_id, :passage_id,
-    :placeref_type, :place_id
+    :placeref_type, :place_id, :year
 
   config.sort_order = 'placeref'
 
